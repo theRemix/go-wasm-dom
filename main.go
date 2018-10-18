ce3f3cb16e66e5893d46563b6a88d2e3c3916e37
Jon Borgonia <jon@gomagames.com>
2018-10-17 21:52:00 -0700

    Add List rendering demo to add 10,000 dom children

--- main.go
+++ main.go
@@ -7,12 +7,21 @@ import (
 	"github.com/dennwc/dom"
 )
 
+func createChildEl(index int) *dom.Element {
+	child := dom.Doc.CreateElement("li")
+
+	span := dom.Doc.CreateElement("span")
+	span.SetTextContent("Child Element " + strconv.Itoa(index))
+	child.AppendChild(span)
+
+	return child
+}
 func main() {
 	fmt.Println("Go Wasm Dom")
 	fmt.Println("stdout prints to console")
 	dom.ConsoleLog("alternatively use dom.ConsoleLog()")
 
-	count := 0
+	// App
 
 	app := dom.Doc.GetElementById("app")
 	app.SetAttribute("class", "container")
@@ -22,8 +31,12 @@ func main() {
 	appHeading.SetAttribute("class", "title")
 	app.AppendChild(appHeading)
 
-	counterContainer := dom.Doc.CreateElement("p")
-	counterContainer.SetAttribute("class", "control")
+	// Counter Demo
+
+	count := 0
+
+	counterContainer := dom.Doc.CreateElement("div")
+	counterContainer.SetAttribute("class", "section")
 	app.AppendChild(counterContainer)
 
 	counterHeading := dom.Doc.CreateElement("h2")
@@ -51,9 +64,50 @@ func main() {
 		counterCh <- count - 1
 	})
 
+	// List Demo
+
+	listChildCount := 10000
+
+	listContainer := dom.Doc.CreateElement("div")
+	listContainer.SetAttribute("class", "section")
+	app.AppendChild(listContainer)
+
+	listHeading := dom.Doc.CreateElement("h2")
+	listHeading.SetTextContent("List ")
+	listHeading.SetAttribute("class", "subtitle")
+	listContainer.AppendChild(listHeading)
+
+	renderListBtn := dom.Doc.NewButton("Render list of " + strconv.Itoa(listChildCount) + " items")
+	renderListBtn.SetAttribute("class", "button is-success is-rounded")
+	listContainer.AppendChild(renderListBtn)
+
+	listUl := dom.Doc.CreateElement("ul")
+	listUl.SetAttribute("id", "list-demo-container")
+	listContainer.AppendChild(listUl)
+
+	renderCh := make(chan int, 1)
+	renderListBtn.OnClick(func(_ dom.Event) {
+		renderCh <- listChildCount
+	})
+
+	// Loop
+
 	for {
-		count = <-counterCh
-		countValue.SetTextContent(strconv.Itoa(count))
+
+		select {
+
+		case count = <-counterCh:
+			countValue.SetTextContent(strconv.Itoa(count))
+
+		case count = <-renderCh:
+			flameChart := dom.Doc.CreateElement("img")
+			flameChart.SetAttribute("src", "./img/flame-chart.png")
+			listContainer.AppendChild(flameChart)
+			for i := 1; i <= count; i++ {
+				listUl.AppendChild(createChildEl(i))
+			}
+
+		}
 	}
 
 	// idk what this does
